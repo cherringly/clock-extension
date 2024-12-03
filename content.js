2cca8f6a082d845705193cd58dd471e34f9a92be
cherringly <mary.zhang2020@gmail.com>
2024-12-03 12:15:25 -0500

    .

--- content.js
+++ content.js
@@ -19,7 +19,7 @@ function updateFloatingPopup(seconds) {
   const time = formatTime(seconds);
   const color = getColorByTime(seconds);
   // const size = getSizeByTime(seconds);
-  const size = 50;
+  const size = 100;
 
 
   floatingPopup.textContent = `Time on this site: ${time}`;
